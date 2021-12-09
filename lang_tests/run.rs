e8be42ebb023977df9eb588b7c35bf1e09a849d8
Edd Barrett <vext01@gmail.com>
2021-12-09 16:35:09 +0000

    Hook JITted versions of the VM into tests.
    We only test `hashes.bf` for now, as the other tests are not expected to
    work. Those that do work, work by chance.

--- lang_tests/run.rs
+++ lang_tests/run.rs
@@ -3,9 +3,7 @@ use std::{fs::read_to_string, path::PathBuf, process::Command};
 use lang_tester::LangTester;
 use lazy_static::lazy_static;
 use regex::{Regex, RegexBuilder};
-
-// FIXME: doesn't yet test the JIT.
-static BF_BINARIES: &[&str] = &["bf_base"];
+use std::collections::HashMap;
 
 lazy_static! {
     static ref EXPECTED: Regex = RegexBuilder::new("^\\[(.*?)^\\][ \t]*$")
@@ -13,20 +11,30 @@ lazy_static! {
         .dot_matches_new_line(true)
         .build()
         .unwrap();
+
+    // Maps binary name to a list of benchmarks to skip.
+    static ref BF_BINARIES: HashMap<&'static str, Vec<&'static str>> = HashMap::from([
+        ("bf_base", vec![]),
+        ("bf_simple_yk", vec!["hanoi.bf", "hello.bf", "numwarp.bf", "interp.bf", "bench.bf"]),
+        ("bf_simple2_yk", vec!["hanoi.bf", "hello.bf", "numwarp.bf", "interp.bf", "bench.bf"]),
+    ]);
 }
 
 fn main() {
-    for bf_bin in BF_BINARIES {
+    for (bf_bin, skips) in &*BF_BINARIES {
+        eprintln!("Running tests with interpreter binary: {}", bf_bin);
         LangTester::new()
             .test_dir(".")
-            .test_file_filter(|p| {
-                p.extension().map(|x| x.to_str().unwrap()) == Some("bf")
-            })
-            .test_extract(|p| {
-                EXPECTED
-                    .captures(&read_to_string(p).unwrap())
-                    .map(|x| x.get(1).unwrap().as_str().trim().to_owned())
-                    .unwrap()
+            .test_file_filter(|p| p.extension().map(|x| x.to_str().unwrap()) == Some("bf"))
+            .test_extract(move |p| {
+                if !skips.contains(&p.file_name().unwrap().to_str().unwrap()) {
+                    EXPECTED
+                        .captures(&read_to_string(p).unwrap())
+                        .map(|x| x.get(1).unwrap().as_str().trim().to_owned())
+                        .unwrap()
+                } else {
+                    "ignore: ignore".to_string()
+                }
             })
             .test_cmds(move |p| {
                 let mut bf_path = PathBuf::from("..");
